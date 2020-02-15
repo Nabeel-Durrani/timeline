8f1c0e2a4fe96522605bca69b22f891c268b42db
Nabeel Durrani <nabeel.durrani@uqconnect.edu.au>
2020-02-15 16:08:04 +1000

    refactored code

--- timeline.py
+++ timeline.py
@@ -38,83 +38,74 @@ class Surface:
             context.show_text(title)
             return self
 class TimeLine:
-    def __init__(self, surface, timeframe=40, coarseness=4,
-                 start=15, month=29, font="Yanone Kaffeesatz Thin", scale=0.7,
-                 fontSize=0.03, tickHeight=0.01, linewidth=0.003,
-                 lineColor=(0, 0, 0)):
+    def __init__(self, surface, translation=(0.25, 0.25), scale=0.7):
         self.surface = surface
-        self.linewidth = linewidth
-        self.lineColor = lineColor
-        self.timeframe = timeframe
-        self.start, self.month = start, month
-        self.tickHeight = tickHeight
-        self.coarseness = coarseness
-        self.scale = scale
-        self.font, self.fontSize = font, fontSize
-    def draw(self, surface):
+        self.translation, self.scale = translation, scale
+    def draw(self, surface, font="Yanone Kaffeesatz Thin", fontSize=0.03,
+             lineWidth=0.003, lineColor=(0, 0, 0), textSep=0.01,
+             coarseness=4, timeframe=40, tickHeight=0.01, start=15, month=29):
         context = self.context()
-        context.select_font_face(self.font)
-        context.set_font_size(self.fontSize)
-        context.set_line_width(self.linewidth)
-        context.set_source_rgb(*self.lineColor)
+        context.select_font_face(font)
+        context.set_font_size(fontSize)
+        context.set_line_width(lineWidth)
+        context.set_source_rgb(*lineColor)
 
         self._horizontal_line(context)
-        self._vertical_steps(context)
+        self._vertical_steps(context, coarseness, timeframe, textSep,
+                             tickHeight, start, month)
+        return self
     def _horizontal_line(self, context):
         context.move_to(0, 0)
         context.line_to(1, 0)
         context.stroke()
-    def _vertical_steps(self, context, textSep=0.01):
-        dx = float(self.coarseness) / self.timeframe
-        for i in range(int(self.timeframe / self.coarseness) + 1):
-            context.move_to(i * dx, -self.tickHeight)
-            context.line_to(i * dx,  self.tickHeight)
+    def _vertical_steps(self, context, coarseness, timeframe, textSep,
+                        tickHeight, start, month):
+        dx = float(coarseness) / timeframe
+        for i in range(int(timeframe / coarseness) + 1):
+            context.move_to(i * dx, -tickHeight)
+            context.line_to(i * dx,  tickHeight)
             context.stroke()
-            context.move_to(i * dx, -self.tickHeight - textSep)
-            context.show_text(str((self.start +
-                                   i * self.coarseness) % self.month))
-    def context(self, width=None, height=None, translate=0.25):
+            context.move_to(i * dx, -tickHeight - textSep)
+            context.show_text(str((start +
+                                   i * coarseness) % month))
+    def context(self, width=None, height=None):
         context = self.surface.context(width, height, scale=self.scale)
-        context.translate(translate, translate)
+        context.translate(*self.translation)
         return context
 class Tasks:
-    def __init__(self, timeline, heading, vPos=0,
-                 linewidth=0.001, font="Yanone Kaffeesatz Thin",
+    def __init__(self, timeline, heading, vPos=0.01,
                  headingFont="Yanone Kaffeesatz Bold",
-                 headingFontSize=0.025, taskFontSize=0.02):
-        self.vPos = vPos
-        self.linewidth = linewidth
-        self.font = font
-        self.headingFontSize, self.taskFontSize = headingFontSize, taskFontSize
-        self.headingFont = headingFont
-        self.set_heading(timeline.context(), heading)
-        self.add_task(timeline.context(),
-                      "Example text 2")
-    def set_heading(self, context, heading, padding=0.03):
+                 headingFontSize=0.025):
+        self.context = self._set_heading(timeline.context(),
+                                         heading, headingFont,
+                                         headingFontSize, vPos)
+    def _set_heading(self, context, heading, headingFont, headingFontSize,
+                     vPos, padding=0.03):
         context.set_source_rgb(0, 0, 0)
-        context.select_font_face(self.headingFont)
-        context.set_font_size(self.headingFontSize)
+        context.select_font_face(headingFont)
+        context.set_font_size(headingFontSize)
         xbearing, ybearing, width, height, dx, dy = \
                 context.text_extents(heading)
-        context.move_to(-width - padding, self.vPos)
+        context.translate(0, vPos)
+        context.move_to(-width - padding, 0)
         context.show_text(heading)
-    def add_task(self, context, text):
-        context.select_font_face(self.font)
-        context.set_font_size(self.taskFontSize)
-        context.set_line_width(self.linewidth)
-        textPos = 0.15
+        return context
+    def add_task(self, text, vPos=0.15, font="Yanone Kaffeesatz Thin",
+                 linewidth=0.001, taskFontSize=0.02):
+        self.context.select_font_face(font)
+        self.context.set_font_size(taskFontSize)
+        self.context.set_line_width(linewidth)
         xbearing, ybearing, width, height, dx, dy = \
-                context.text_extents(text)
-        context.move_to(0, 0)
-        context.line_to(0, textPos - height)
-        context.stroke()
-        context.move_to(0, textPos)
-        context.show_text(text)
+                self.context.text_extents(text)
+        self.context.move_to(0, 0)
+        self.context.line_to(0, vPos - height)
+        self.context.stroke()
+        self.context.move_to(0, vPos)
+        self.context.show_text(text)
+        return self.context
 def main():
     with Surface() as surface:
-        timeline = TimeLine(surface.add_title(surface))
-        Tasks(timeline, "Example Text 1")
-        timeline.draw(surface)
-
+        Tasks(TimeLine(surface.add_title(surface)).draw(surface),
+              "Example Text 1").add_task("Example Text 2")
 if __name__ == "__main__":
     main()
