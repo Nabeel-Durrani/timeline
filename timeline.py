5f04b22abd9781b97bae06bcfad1d2f7022741d9
Nabeel Durrani <nabeel.durrani@uqconnect.edu.au>
2020-02-17 10:45:26 +1000

    proper handling of dates implemented using datetime library

--- timeline.py
+++ timeline.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 import cairo
+import calendar
+import datetime
+from dateutil.rrule import rrule, MONTHLY, DAILY
 
 class Surface:
     def __init__(self, bgcolor=(1, 1, 1), fname="out",
@@ -43,7 +46,9 @@ class TimeLine:
         self.translation, self.scale = translation, scale
     def draw(self, surface, font="Yanone Kaffeesatz Thin", fontSize=0.03,
              lineWidth=0.003, lineColor=(0, 0, 0), textSep=0.01,
-             coarseness=4, timeframe=40, tickHeight=0.01, start=15, month=29):
+             coarseness=4, timeframe=datetime.timedelta(days=50),
+             tickHeight=0.01, start=datetime.date(2020, 2, 15),
+             monthTextSep=0.04):
         context = self.context()
         context.select_font_face(font)
         context.set_font_size(fontSize)
@@ -52,33 +57,40 @@ class TimeLine:
 
         self._horizontal_line(context)
         self._vertical_steps(context, coarseness, timeframe, textSep,
-                             tickHeight, start, month)
+                             tickHeight, start, monthTextSep)
         return self
     def _horizontal_line(self, context):
         context.move_to(0, 0)
         context.line_to(1, 0)
         context.stroke()
     def _vertical_steps(self, context, coarseness, timeframe, textSep,
-                        tickHeight, start, month):
-        dx = float(coarseness) / timeframe
-        for i in range(int(timeframe / coarseness) + 1):
+                        tickHeight, start, monthTextSep):
+        dt = 1 / float(timeframe.days)
+        dx = float(coarseness) * dt
+        days = [(t.day, t.month) for t in rrule(DAILY, interval=coarseness,
+                                                dtstart=start,
+                                                until=(start + timeframe))]
+        for i in range(len(days)):
             context.move_to(i * dx, -tickHeight)
             context.line_to(i * dx,  tickHeight)
             context.stroke()
             context.move_to(i * dx, -tickHeight - textSep)
-            context.show_text(str((start +
-                                   i * coarseness) % month))
+            context.show_text(str(days[i][0]))
+            context.move_to(i * dx, -tickHeight - textSep - monthTextSep)
+            if i - 1 < 0 or days[i - 1][1] < days[i][1]:
+                context.show_text(calendar.month_abbr[days[i][1]])
     def context(self, width=None, height=None):
         context = self.surface.context(width, height, scale=self.scale)
         context.translate(*self.translation)
         return context
 class Tasks:
-    def __init__(self, timeline, heading, vPos=0.01,
+    def __init__(self, timeline, heading, vPos=0.05,
                  headingFont="Yanone Kaffeesatz Bold",
                  headingFontSize=0.025):
         self.context = self._set_heading(timeline.context(),
                                          heading, headingFont,
                                          headingFontSize, vPos)
+        self.height = 0
     def _set_heading(self, context, heading, headingFont, headingFontSize,
                      vPos, padding=0.03):
         context.set_source_rgb(0, 0, 0)
@@ -92,20 +104,24 @@ class Tasks:
         return context
     def add_task(self, text, vPos=0.15, font="Yanone Kaffeesatz Thin",
                  linewidth=0.001, taskFontSize=0.02):
+        y = vPos + self.height
+        self.height += vPos
+
         self.context.select_font_face(font)
         self.context.set_font_size(taskFontSize)
         self.context.set_line_width(linewidth)
         xbearing, ybearing, width, height, dx, dy = \
                 self.context.text_extents(text)
         self.context.move_to(0, 0)
-        self.context.line_to(0, vPos - height)
+        self.context.line_to(0, y - height)
         self.context.stroke()
-        self.context.move_to(0, vPos)
+        self.context.move_to(0, y)
         self.context.show_text(text)
-        return self.context
+        return self
 def main():
     with Surface() as surface:
-        Tasks(TimeLine(surface.add_title(surface)).draw(surface),
-              "Example Text 1").add_task("Example Text 2")
+        timeline = TimeLine(surface.add_title(surface)).draw(surface)
+        Tasks(timeline, "Heading 1").add_task("Text 1A").add_task("Text 1B")
+        Tasks(timeline, "Heading 2", vPos=0.4).add_task("Text 2A").add_task("Text2B")
 if __name__ == "__main__":
     main()
